18ed9bf973675dda084f1a79455dc685cd9a500d
nvtruong02 <nvtruong02@gmail.com>
2017-08-15 21:05:45 +0700

    TDD and Nunit

---
+++ Bowling/BowlingUnitTest.cs
@@ -0,0 +1,143 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bowling
+{
+    [TestFixture]
+    class BowlingUnitTest
+    {
+        Game game;
+        [SetUp]
+        public void CreateGame()
+        {
+            game = new Game();
+        }
+
+        private void RollSpare()
+        {
+            game.Roll(5);
+            game.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            game.Roll(10);
+        }
+        private void ManyRolls(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+                game.Roll(pins);
+        }
+
+        [Test]
+        public void Rolls()
+        {
+            ManyRolls(20, 1);
+
+            Assert.That(game.Score(), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void OneSpare()
+        {
+            RollSpare();
+            game.Roll(4);
+            game.Roll(2);
+            ManyRolls(16, 0);
+
+            Assert.That(game.Score(), Is.EqualTo(20));
+        }
+
+        [Test]
+        public void SpareSpare()
+        {
+            RollSpare();
+            RollSpare();
+            game.Roll(4);
+            game.Roll(2);
+            ManyRolls(14, 0);
+
+            Assert.That(game.Score(), Is.EqualTo(35));
+        }
+
+        [Test]
+        public void OneStrike()
+        {
+            RollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            ManyRolls(16, 0);
+
+            Assert.That(game.Score(), Is.EqualTo(24));
+        }
+
+        [Test]
+        public void Double()
+        {
+            RollStrike();
+            RollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            ManyRolls(14, 0);
+
+            Assert.That(game.Score(), Is.EqualTo(47));
+        }
+
+        [Test]
+        public void Turkey()
+        {
+            RollStrike();
+            RollStrike();
+            RollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            ManyRolls(12, 0);
+
+            Assert.That(game.Score(), Is.EqualTo(77));
+        }
+
+        [Test]
+        public void StrikeSpareStrike()
+        {
+            RollStrike();
+            RollSpare();
+            RollStrike();
+            game.Roll(3);
+            game.Roll(4);
+            ManyRolls(12, 0);
+
+            Assert.That(game.Score(), Is.EqualTo(64));
+        }
+
+        [Test]
+        public void LastTwoSpare()
+        {
+            ManyRolls(16, 0);
+            RollSpare();
+            RollSpare();
+
+            Assert.That(game.Score(), Is.EqualTo(25));
+        }
+
+        [Test]
+        public void LastTwoStrike()
+        {
+            ManyRolls(16, 0);
+            RollStrike();
+            RollStrike();
+           
+            Assert.That(game.Score(), Is.EqualTo(30));
+        }
+
+        [Test]
+        public void PerfectGame()
+        {
+            ManyRolls(12, 10);
+            Assert.That(game.Score(), Is.EqualTo(300));
+        }
+    }
+}
---
+++ Bowling/Game.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Bowling
+{
+    class Game
+    {
+        private const int perfect = 10;
+        
+        private List<int> lstPins = new List<int>();
+        public void Roll(int pins)
+        {
+            lstPins.Add(pins);
+        }
+
+        public int Score()
+        {
+            int score = 0;
+            int sessionRoll = 0;
+            for (int frame = 0; frame < 10;frame++ )
+            {
+                if (IsStrike(sessionRoll))
+                {
+                    score += 10 + GetNextTwoPinsForStrike(sessionRoll);
+                    sessionRoll += 1;
+                }
+                else if (IsSpare(sessionRoll))
+                {
+                    score += 10 + GetNextPinForSpare(sessionRoll);
+                    sessionRoll += 2;
+                }
+                else
+                {
+                    score += GetTwoPinInFrame(sessionRoll);
+                    sessionRoll += 2;
+                }
+            }
+
+            return score;
+        }
+
+        private int GetNextTwoPinsForStrike(int sessionRoll)
+        {
+            return GetPinOfNextSession(sessionRoll) + GetPinOfTwoNextSession(sessionRoll);
+        }
+
+        private int GetPinOfTwoNextSession(int sessionRoll)
+        {
+            int twoNextSession = 0;
+            if (IsSessionNotOutListPins(sessionRoll + 2))
+                twoNextSession = lstPins[sessionRoll + 2];
+            return twoNextSession;
+        }
+
+        private int GetPinOfNextSession(int sessionRoll)
+        {
+            int nextSession = 0;
+            if (IsSessionNotOutListPins(sessionRoll + 1))
+                nextSession = lstPins[sessionRoll + 1];
+            return nextSession;
+        }
+
+        private bool IsSessionNotOutListPins(int sessionCheck)
+        {
+            return lstPins.Count() > sessionCheck;
+        }
+
+        private bool IsStrike(int sessionRoll)
+        {
+            return lstPins[sessionRoll] == perfect;
+        }
+
+        private int GetTwoPinInFrame(int sessionRoll)
+        {
+            return GetPinOfSession(sessionRoll) + GetPinOfNextSession(sessionRoll);
+        }
+
+        private int GetPinOfSession(int sessionRoll)
+        {
+            return lstPins[sessionRoll];
+        }
+
+        private int GetNextPinForSpare(int sessionRoll)
+        {
+            return GetPinOfTwoNextSession(sessionRoll);
+        }
+
+        private bool IsSpare(int sessionRoll)
+        {
+            return lstPins[sessionRoll] + lstPins[sessionRoll + 1] == perfect;
+        }
+    }
+}
